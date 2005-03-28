bf7c81b900934fa4f044c6def2f63a419db8dc3b
VMiklos <vmiklos@inflame.hu>
2005-03-28 21:22:35 +0000

    config kommentelesek miutan mar ense tudtam, h mi hogy smint

--- config.php
+++ config.php
@@ -1,14 +1,16 @@
 <?
-$mysql_host = 'localhost';
+$mysql_host = 'localhost'; // mysql connect paramok
 $mysql_user = 'openblog';
 $mysql_pass = 'ballaballa';
 $mysql_db   = 'openblog';
 
 $site_root  = "";
-$date_format='%Y%m';
+$date_format='%Y %M';
+$date_format_display="%Y %B";
 
 setlocale (LC_TIME, "hu_HU");
 $postcim_lenght = 5;
+$postidezet_lenght = 23;
 $users_limit=5;
 $posts_limit=5;
 $news_limit=3;
