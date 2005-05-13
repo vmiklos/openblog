1c3ad0097003d32cf17ffcffa3fb85afcfae2bab
VMiklos <vmiklos@inflame.hu>
2005-05-13 19:09:27 +0000

    gombok elrejtese szukitett feltetelek eseten

--- templates/user_list.php
+++ templates/user_list.php
@@ -2,12 +2,12 @@
 foreach($users as $i)
 	print("\t<li><a href=\"/" . $i['name'] . "\">" . name2nick($i['name']) .
 		"</a></li>\n");
-if ($id != $prev)
+if ($id != $prev and is_null($sterm))
 {
 	print("<a href=\"/list/$pprev\">&lt;&lt;- eleje</a> ");
 	print("<a href=\"/list/$prev\">&lt;- elozo</a><br>");
 }
-if ($id != $next)
+if ($id != $next and is_null($sterm))
 {
 	print("<a href=\"/list/$next\">kovetkezo -&gt;</a> ");
 	print("<a href=\"/list/$nnext\">vege -&gt;&gt;</a>");
