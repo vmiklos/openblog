e652f5391281d2442fcaeaf2a6192470648998db
VMiklos <vmiklos@inflame.hu>
2005-05-13 18:39:08 +0000

    ehh, es a hozza tartozo sablonok

---
+++ templates/user_list.php
@@ -0,0 +1,9 @@
+<?
+foreach($users as $i)
+	print("\t<li><a href=\"/" . $i['name'] . "\">" . name2nick($i['name']) .
+		"</a></li>\n");
+if ($id != $prev)
+	print("<a href=\"/list/$prev\">&lt;- elozo</a><br>");
+if ($id != $next)
+	print("<a href=\"/list/$next\">kovetkezo -&gt;</a>");
+?>
